00a9ff6f4e4646388377d05ff32bc249ca0ab6e6
LoicGoulefert <loic.goulefert31@gmail.com>
2020-04-12 14:49:53 +0200

    Add 4D noise for loopy gifs !

--- perlin.py
+++ perlin.py
@@ -1,46 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 import imageio
-from noise.perlin import SimplexNoise
+import numpy as np
 from noise import snoise2, snoise3, snoise4
-
-
-def perlin2D(shape, res):
-    def smoothstep(t):
-        return 6*t**5 - 15*t**4 + 10*t**3
-    
-    dim = len(shape)
-    
-    delta = tuple(res[i] / shape[i] for i in range(dim))
-    d = tuple(shape[i] // res[i] for i in range(dim))
-
-    mgrid_slices = [slice(0, res[i], delta[i]) for i in range(dim)]
-    grid = np.mgrid[mgrid_slices]
-
-    transpose_args = list(range(1, dim+1))
-    transpose_args.append(0) 
-    grid = grid.transpose(transpose_args) % 1
-
-    # Gradients
-    # angles = 2*np.pi*np.random.rand(*[res[i]+1 for i in range(dim)])
-    gradients = np.random.rand(*[res[i]+1 for i in range(dim)], dim)
-    g00 = gradients[0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1)
-    g10 = gradients[1:,0:-1].repeat(d[0], 0).repeat(d[1], 1)
-    g01 = gradients[0:-1,1:].repeat(d[0], 0).repeat(d[1], 1)
-    g11 = gradients[1:,1:].repeat(d[0], 0).repeat(d[1], 1)
-
-    # Ramps
-    n00 = np.sum(grid * g00, 2)
-    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1])) * g10, 2)
-    n01 = np.sum(np.dstack((grid[:,:,0], grid[:,:,1]-1)) * g01, 2)
-    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
-
-    # Interpolation
-    t = smoothstep(grid)
-    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
-    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
-
-    return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
 
 
 def simplex_noise3d(shape, scale, octaves=1):
@@ -53,49 +14,43 @@ def simplex_noise3d(shape, scale, octaves=1):
     return img
 
 
-def make_2d_gif(N, scale, frames, octaves=1, fps=30, output='perlin2d.gif'):
-    shape = (N, N)
-    images = []
-
-    for i in range(frames):
-        images.append(simplex_noise2d(shape, scale, octaves))
-        noise.randomize()
+def simplex_noise4d(shape, scale, radius=1.5, octaves=1):
+    img = np.zeros(shape)
+    for i in range(shape[2]):
+        for x in range(shape[0]):
+            for y in range(shape[1]):
+                cos_value = radius * math.cos(2 * math.pi * (i / shape[2]))
+                sin_value = radius * math.sin(2 * math.pi * (i / shape[2]))
+                img[x, y, i] = snoise4(x * scale[0], y * scale[1], cos_value, sin_value)
+                img[x, y, i] = img[x, y, i] * 128.0 + 127.0
     
-    # Range [0, 255]
-    images = [(255 * (image - np.min(image)) / np.ptp(image)).astype('uint8') for image in images]
-
-    kwargs = {'duration': 1 / fps}
-    imageio.mimsave(output, images, **kwargs)
+    return img
 
 
-def make_3d_gif(N, scale, frames, octaves=1, fps=30, output='perlin3d.gif'):
-    shape = (N, N, frames)
-    
-    images = simplex_noise3d(shape, scale)
+def make_3d_gif(N, shape, scale, frames, octaves=1, fps=30, output='perlin3d.gif'):
+    images = simplex_noise3d(shape, scale, octaves=octaves)
     images = images.astype('uint8')
-    
     images = images.transpose(2, 0, 1)
 
     kwargs = {'duration': 1 / fps}
     imageio.mimsave(output, images, **kwargs)
 
 
-def make_4d_gif(N, scale, frames, octaves=1, fps=30, output='perlin3d.gif'):
-    shape = (N, N, frames)
-    
-    images = simplex_noise3d(shape, scale)
+def make_4d_gif(N, shape, scale, frames, octaves=1, radius=1.5, fps=30, output='perlin3d.gif'):
+    images = simplex_noise4d(shape, scale, octaves=octaves, radius=radius)
     images = images.astype('uint8')
-    
     images = images.transpose(2, 0, 1)
 
     kwargs = {'duration': 1 / fps}
     imageio.mimsave(output, images, **kwargs)
 
-
+ 
 if __name__ == "__main__":
     N = 512
-    scale2D = (0.01, 0.01)
-    scale3D = (0.005, 0.005, 0.02)
-    frames = 60
+    frames = 90
+    shape = (N, N, frames)
+    scale4D = (0.005, 0.005)
+    octaves = 1
+    radius = 0.5
 
-    make_3d_gif(N, scale3D, frames)
+    make_4d_gif(N, shape, scale4D, frames, octaves=octaves, radius=radius)
