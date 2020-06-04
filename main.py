a9edf89149614748767f10d405919c13fc34b0e6
Loic Goulefert <loic.goulefert31@gmail.com>
2020-06-04 22:47:34 +0200

    Got rid of the CLI, using a dict for configuration now

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 import argparse
 import numpy as np
 
-from perlin import PerlinGif
+from perlin import PerlinGif, PerlinFlowField
 from postprocessing import Pipeline, AdjustBrightness, Quantize, Border, Mask, concentric_rectangle_mask
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="CLI tool to create perlin noise gifs")
-    parser.add_argument("-d", type=int, choices=[3, 4], help="noise dimension", default=4)
-    parser.add_argument("-n", type=int, nargs='+', help="specify the gif dimension", default=(256, 256))
-    parser.add_argument("-fps", type=int, help="specify the framerate", default=30)
-    parser.add_argument("-frames", type=int, help="how many frames in the gif", default=30)
-    parser.add_argument("-s", type=float, nargs='+', help="specify the scale (tuple of floats in the [0, 1] range)", default=(0.01, 0.01))
-    parser.add_argument("-o", type=int, choices=[1, 2, 3, 4], help="how many octaves to use", default=1)
-    parser.add_argument("-r", type=float, help="radius (for 4D noise)", default=0.1)
-    parser.add_argument("-c", "--compress", action="store_true", help="set this flag to enable gif compression", default=False)
-    parser.add_argument("-out", type=str, help="output file name (will be created)", default="out.gif")
-    parser.add_argument("-R", action="store_true", help="set this flag to use a random starting point in the noise function", default=False)
 
-    args = vars(parser.parse_args())
-
+    gif_config = {
+        "noise_dimension": 4,
+        "size": (256, 256),
+        "fps": 30,
+        "frames": 30,
+        "scale": (0.01, 0.01),
+        "octaves": 1,
+        "radius": 0.1,
+        "compression": False,
+        "output_file": "out.gif",
+        "random_seed": False,
+        "pipeline": Pipeline()
+        # pipeline = Pipeline(AdjustBrightness(gamma=0.5), Quantize(bins=20), Border(margin=60, width=4))
+    }
+    
     # Sanity check
-    if args['d'] == 3:
-        assert len(args['s']) == 3, "3 dimension scale needed for 3D noise. Got {} ({}D).".format(args['s'], len(args['s']))
-
-    # Create post-processing pipeline
-    pipeline = Pipeline(AdjustBrightness(gamma=0.5), Quantize(bins=20), Border(margin=60, width=4))
-    # pipeline = Pipeline()
-    args['pipeline'] = pipeline
+    if gif_config['noise_dimension'] == 3:
+        assert len(gif_config['scale']) == 3, \
+        "3 dimension scale needed for 3D noise. Got {} ({}D).".format(gif_config['scale'], len(gif_config['scale']))
 
     # Render gif
-    pg = PerlinGif(**args)
+    pg = PerlinGif(**gif_config)
+    # pg = PerlinFlowField(**gif_config)
     pg.render()
--- perlin.py
+++ perlin.py
@@ -46,50 +46,51 @@ def _simplex_noise4d(shape, scale, octaves, radius, random):
     return img
 
 
-class PerlinGif():
-    def __init__(self, **kwargs):
-        self.noise_dim = kwargs["d"]
-        self.n = kwargs["n"]
-        self.fps = kwargs["fps"]
-        self.frames = kwargs["frames"]
-        self.scale = kwargs["s"]
-        self.octaves = kwargs["o"]
-        self.radius = kwargs["r"]
-        self.compress = kwargs["compress"]
-        self.output_file = kwargs["out"]
-        self.pipeline = kwargs["pipeline"]
-        self.random = kwargs["R"]
-        self.shape = (*self.n, self.frames)
-    
-    def _to_gif(self):
-        kwargs = {'duration': 1 / self.fps}
-        imageio.mimsave(self.output_file, self.images, **kwargs)
-        if self.compress:
-            optimize(self.output_file)
+def _to_gif(images, fps, output_file, compression):
+    kwargs = {'duration': 1 / fps}
+    imageio.mimsave(output_file, images, **kwargs)
+    if compression:
+        optimize(output_file)
 
-    def _make_3d_gif(self):
-        images = _simplex_noise3d(self.shape, self.scale, self.octaves, self.random)
+
+class PerlinGif():
+    def __init__(self, **config):
+        self.noise_dimension = config["noise_dimension"]
+        self.size = config["size"]
+        self.fps = config["fps"]
+        self.frames = config["frames"]
+        self.scale = config["scale"]
+        self.octaves = config["octaves"]
+        self.radius = config["radius"]
+        self.compression = config["compression"]
+        self.output_file = config["output_file"]
+        self.pipeline = config["pipeline"]
+        self.random_seed = config["random_seed"]
+        self.shape = (*self.size, self.frames)
+
+    def _make_3d_images(self):
+        images = _simplex_noise3d(self.shape, self.scale, self.octaves, self.random_seed)
         images = images.transpose(2, 0, 1)
 
         return images
-        
-    def _make_4d_gif(self):
-        images = _simplex_noise4d(self.shape, self.scale, self.octaves, self.radius, self.random)
+
+    def _make_4d_images(self):
+        images = _simplex_noise4d(self.shape, self.scale, self.octaves, self.radius, self.random_seed)
         images = images.transpose(2, 0, 1)
-    
+
         return images
 
     def render(self):
-        if self.noise_dim == 3:
-            self.images = self._make_3d_gif()
+        if self.noise_dimension == 3:
+            self.images = self._make_3d_images()
         else:
-            self.images = self._make_4d_gif()
-        
+            self.images = self._make_4d_images()
+
         if not self.pipeline.is_empty():
             print("Running pipeline")
             self.images = self.pipeline.run(self.images)
-        
-        self._to_gif()
+
+        _to_gif(self.images, self.fps, self.output_file, self.compression)
 
 
 class PerlinFlowField():
@@ -106,18 +107,18 @@ class PerlinFlowField():
         self.pipeline = kwargs["pipeline"]
         self.random = kwargs["R"]
         self.shape = (*self.n, self.frames)
+        self.images = np.zeros(self.shape)
 
     def flow_field(self):
-        # generate vector field
-        flow_field = np.zeros((*shape, 2)) # if 256, 256, 30 -> 256, 256, 30, 2
+        flow_field = np.zeros((*self.shape, 2)) # if 256, 256, 30 -> 256, 256, 30, 2
         radius = 0.1
         scale = (0.1, 0.1)
 
-        for i in range(shape[2]):
-            cos_value = self.radius * math.cos(2 * math.pi * (i / shape[2]))
-            sin_value = self.radius * math.sin(2 * math.pi * (i / shape[2]))
-            for x in range(shape[0]):
-                for y in range(shape[1]):
+        for i in range(self.shape[2]):
+            cos_value = self.radius * math.cos(2 * math.pi * (i / self.shape[2]))
+            sin_value = self.radius * math.sin(2 * math.pi * (i / self.shape[2]))
+            for x in range(self.shape[0]):
+                for y in range(self.shape[1]):
                     angle = snoise4(x * self.scale[0], y * self.scale[1], cos_value, sin_value)
                     flow_field[x, y, i, 0] = np.cos(angle) * 10
                     flow_field[x, y, i, 1] = np.sin(angle) * 10
@@ -131,16 +132,87 @@ class PerlinFlowField():
         # axs[1][0].quiver(x, y, flow_field[:, :, 2, 0], flow_field[:, :, 2, 1])
         # axs[1][1].quiver(x, y, flow_field[:, :, 3, 0], flow_field[:, :, 3, 1])
         # plt.show()
+        self.flow_field = flow_field
 
-    def add_particles(self):
-        # add particles at random location
-        pass
+        return self.flow_field
 
-    def update_positions(self):
+    def add_particles(self, n_particles):
+        # add particles at random location along the y=0 axis
+        self.particles = []
+        rand_x = np.random.randint(0, self.shape[0], size=n_particles)
+        for x in rand_x:
+            pos = Vec2D(x, 0)
+            vel = Vec2D(0, 0)
+            acc = Vec2D(0, 0)
+            self.particles.append(Particle(pos, vel, acc))
+
+    def update_particles(self):        
         # for each particle, find associated vector, apply force
-        # Make sure they wrap around the frame
+        for i in range(self.frames):
+            for particle in self.particles:
+                force = Vec2D(*self.flow_field[round(particle.pos.x), round(particle.pos.y), i, :])
+                particle.apply(force)
+
+                # Wrap particle around frame
+                if particle.pos.x > self.shape[0]:
+                    particle.pos.x = 0
+                if particle.pos.x < 0:
+                    particle.pos.x = self.shape[0] - 1
+                if particle.pos.y > self.shape[1]:
+                    particle.pos.y = 0
+                if particle.pos.y < 0:
+                    particle.pos.y = self.shape[1] - 1
+            
+            self._draw_frame(i)
+        self.images = ((self.images - self.images.min()) * (1 / (self.images.max() - self.images.min()) * 255)).astype('uint8')
+
+    def _draw_frame(self, index):
+        for particle in self.particles:
+            x = particle.pos.x
+            y = particle.pos.y
+            self.images[x, y, index] = 1
+    
+    def render(self):
         pass
 
-    def draw_frame(self):
-        # Draw each particle as a point
-        pass
+    def _to_gif(self):
+        kwargs = {'duration': 1 / self.fps}
+        imageio.mimsave(self.output_file, self.images, **kwargs)
+        if self.compress:
+            optimize(self.output_file)
+
+
+class Particle():
+    def __init__(self, pos, velocity, acceleration):
+        self.pos = Vec2D(0, 0)
+        self.velocity = Vec2D(0, 0)
+        self.acceleration = Vec2D(0, 0)
+
+    def update(self):
+        self.velocity += self.acceleration
+        self.position += self.velocity
+        self.acceleration.reset()
+
+    def apply(self, force):
+        print("Acc =", self.acceleration)
+        self.acceleration += force
+    
+    def __repr__(self):
+        return f"Particle(pos={self.pos}, vel={self.velocity}, acc={self.acceleration})"
+
+
+class Vec2D():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        self.x += other.x
+        self.y += other.y
+    
+    def __repr__(self):
+        return f"Vec2D(x={self.x}, y={self.y})"
+
+    def reset(self):
+        self.x = 0
+        self.y = 0
